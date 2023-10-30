ae8cad9ba3ac2d51128e00c07fbd65703e3258c2
Luis Eduardo Soto García <luiseduardosg@ufps.edu.co>
2023-10-30 15:05:40 -0500

    Prueba

---
+++ js/json.js
@@ -0,0 +1,7 @@
+https://siaweb-nodejs.carlos-reneren7.repl.co/productos
+
+get /productos: obtengo la relacion de productos
+get /productos/nombre_producto: obtengo producto por nombre
+post /productos: Registra producto nuevo
+put /productos/nombre_producto: actualiza un producto por nomre
+delete /productos/nombre_producto: elimina producto por nombre
